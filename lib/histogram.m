3b77769c22b08db29c5dc24c1b695d92f6a704b8
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-08-30 23:57:27 +0800

    Add gitignore file.

--- lib/histogram.m
+++ lib/histogram.m
@@ -11,24 +11,3 @@ function count = histogram(img,normalize=0)
         count = 1.0*count/(row*col);
     endif
 endfunction
-
-%function img = asciiTOgrayscale(asciiImg)
-    %[row,col] = size(asciiImg);
-    %img = zeros(row,col,'uint8');
-    %for r = 1:row
-        %for c = 1:col
-            %img(r,c) = str2ascii(asciiImg(r,c));
-        %endfor
-    %endfor
-%endfunction
-
-
-%def rotate(img,angle):
-%
-%
-%def otsuThreshold(img):
-%
-%
-%def kapurThreshold(img):
-
-
