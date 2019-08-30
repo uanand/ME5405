df3bf0439e601affa62ce9b0112d50930c9a8e0d
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-08-30 23:49:18 +0800

    Function to read text image and convert characters to ascii code.

---
+++ lib/textTOascii.m
@@ -0,0 +1,29 @@
+function img = textTOascii(text)
+    [~,numElements] = size(text);
+    listImg = [];
+    for c = 1:numElements
+        char = toascii(text(c));
+        if !(char==10 || char==13)
+            listImg = [listImg,char];
+        endif
+    endfor
+    [~,numElements] = size(listImg);
+    row=64; col=64;
+    img = zeros(row,col,'uint8');
+    for r = 1:row
+        for c = 1:col
+            img(r,c) = listImg((r-1)*col + c);
+        endfor
+    endfor
+endfunction
+
+
+%def rotate(img,angle):
+%
+%
+%def otsuThreshold(img):
+%
+%
+%def kapurThreshold(img):
+
+
