1ed15789f1a6f0977c95e82556203754d2122d97
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-09-12 15:21:57 +0800

    Add function for histogram equalization.

---
+++ lib/histogramEqualize.m
@@ -0,0 +1,21 @@
+function [imgEq,histEq] = histogramEqualize(img)
+    printf("Performing Histogram Equalization\n");
+    [row,col] = size(img);
+    imgEq = zeros(row,col,"uint8");
+    H = zeros(1,256);
+    histCum = zeros(1,256);
+    hist = histogram(img);
+    for i = 1:256;
+        totalCount = 0;
+        for j = 1:i;
+            totalCount = totalCount + hist(j);
+        endfor
+        histCum(i+1) = totalCount;
+    endfor
+    for r = 1:row;
+        for c = 1:col;
+            imgEq(r,c) = round(255.0/double(row*col) * double(histCum(img(r,c)+1)));
+        endfor
+    endfor
+    histEq = histogram(imgEq);
+endfunction
