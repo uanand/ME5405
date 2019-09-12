63a623c9b3e710223d6e3065244153bc1837ebff
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-09-12 15:22:33 +0800

    Call histogram equalization from main function.

--- main.m
+++ main.m
@@ -2,14 +2,19 @@ clear;
 addpath('./lib');
 
 # READING AND PROCESSING BITMAP IMAGE
-%img = imread('charact2.bmp');
-%img = img(:,:,1);
-%count = histogram(img,normalize=0);
+img = imread('charact2.bmp');
+img = img(:,:,1);
+count = histogram(img,normalize=0);
+[imgHistEq,histEq] = histogramEqualize(img);
+subplot(2,2,1), imshow(img);
+subplot(2,2,2), imshow(imgHistEq);
+subplot(2,2,3), bar(0:255,count);
+subplot(2,2,4), bar(0:255,histEq);
 
 # READING AND PROCESSING TEXT IMAGE
-textImg = fileread('charact1.txt');
-img = textTOascii(textImg);
-%imwrite(img,'test.png');
-imgStretch = stretchContrast(img);
-imwrite(imgStretch, 'test2.png');
-%imshow(img);
+%textImg = fileread('charact1.txt');
+%img = textTOascii(textImg);
+%%imwrite(img,'test.png');
+%imgStretch = stretchContrast(img);
+%imwrite(imgStretch, 'test2.png');
+%%imshow(img);
