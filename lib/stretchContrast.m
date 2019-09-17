4cf4e239b2873eb87a3dddefcda731d2a8af6350
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-09-17 14:38:35 +0800

    Change gray image datatype to uint8.

--- lib/stretchContrast.m
+++ lib/stretchContrast.m
@@ -15,7 +15,7 @@ function imgStretch = stretchContrast(img,min=0,max=255)
     
     alpha = 255.0/double(intensityMax-intensityMin);
     beta = -alpha*double(intensityMin);
-    imgStretch = zeros(row,col);
+    imgStretch = zeros(row,col,"uint8");
     for r = 1:row
         for c = 1:col
             imgStretch(r,c) = round(alpha*double(img(r,c)) + beta);
--- main.m
+++ main.m
@@ -2,15 +2,19 @@ clear;
 addpath('./lib');
 
 # READING AND PROCESSING BITMAP IMAGE
-%img = imread('charact2.bmp');
-%img = img(:,:,1);
+img = imread('charact2.bmp');
+img = img(:,:,1);
+imgStretch = stretchContrast(img);
+[imgEq,histEq] = histogramEqualize(img);
 %hist = histogram(img);
 %[bImg1,th1] = threshold(img,method="median");
 %[bImg2,th2] = threshold(img,method="mean");
 %[bImg3,th3] = threshold(img,method="otsu");
 %[bImg4,th4] = threshold(img,method="maxentropy");
-%subplot(1,2,1), imshow(img);
-%subplot(1,2,2), imshow(bImg);
+subplot(2,2,1), imshow(img);
+subplot(2,2,2), imshow(imgStretch);
+subplot(2,2,3), imshow(imgEq);
+%imwrite(img)
 
 %count = histogram(img,normalize=0);
 %[imgHistEq,histEq] = histogramEqualize(img);
@@ -20,12 +24,12 @@ addpath('./lib');
 %subplot(2,2,4), bar(0:255,histEq);
 
 # READING AND PROCESSING TEXT IMAGE
-textImg = fileread('charact1.txt');
-img = textTOascii(textImg);
-[bImg1,th1] = threshold(img,method="median");
-[bImg2,th2] = threshold(img,method="mean");
-[bImg3,th3] = threshold(img,method="otsu");
-[bImg4,th4] = threshold(img,method="maxentropy");
+%textImg = fileread('charact1.txt');
+%img = textTOascii(textImg);
+%[bImg1,th1] = threshold(img,method="median");
+%[bImg2,th2] = threshold(img,method="mean");
+%[bImg3,th3] = threshold(img,method="otsu");
+%[bImg4,th4] = threshold(img,method="maxentropy");
 %%imwrite(img,'test.png');
 %imgStretch = stretchContrast(img);
 %imwrite(imgStretch, 'test2.png');
