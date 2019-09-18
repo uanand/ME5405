a86bc483c21b310cac591b663c8be07d0caf6e55
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-09-18 23:59:46 +0800

    Bicubic interpolation is buggy. Need to fix.

--- main.m
+++ main.m
@@ -2,12 +2,10 @@ clear;
 addpath('./lib');
 
 # READING AND PROCESSING BITMAP IMAGE
-img = imread('charact2.bmp');
-img = img(:,:,1);
-%scaleImg1 = imageScale(img,targetRow=200,targetCol=400,method='nearestNeighbor');
-%scaleImg2 = imageScale(img,targetRow=200,targetCol=400,method='bilinear');
-scaleImg3 = imageScale(img,targetRow=200,targetCol=400,method='bicubic');
-scaleImg4 = imresize(img,[200,400],method='bicubic');
+%img = imread('charact2.bmp');
+%img = img(:,:,1);
+
+%scaleImg4 = imresize(img,[200,400],method='bicubic');
 %scaleImg = imageScale(rand(20,20),targetRow=50,targetCol=100,method='bicubic');
 
 %imgStretch = stretchContrast(img);
@@ -17,10 +15,7 @@ scaleImg4 = imresize(img,[200,400],method='bicubic');
 %[bImg2,th2] = threshold(img,method="mean");
 %[bImg3,th3] = threshold(img,method="otsu");
 %[bImg4,th4] = threshold(img,method="maxentropy");
-%subplot(2,2,1), imshow(img);
-%subplot(2,2,2), imshow(scaleImg1);
-%subplot(2,2,3), imshow(scaleImg2);
-%subplot(2,2,4), imshow(scaleImg3);
+
 %imwrite(img)
 
 %count = histogram(img,normalize=0);
@@ -31,8 +26,15 @@ scaleImg4 = imresize(img,[200,400],method='bicubic');
 %subplot(2,2,4), bar(0:255,histEq);
 
 # READING AND PROCESSING TEXT IMAGE
-%textImg = fileread('charact1.txt');
-%img = textTOascii(textImg);
+textImg = fileread('charact1.txt');
+img = textTOascii(textImg);
+scaleImg1 = imageScale(img,targetRow=100,targetCol=150,method='nearestNeighbor');
+scaleImg2 = imageScale(img,targetRow=100,targetCol=150,method='bilinear');
+scaleImg3 = imageScale(img,targetRow=100,targetCol=150,method='bicubic');
+subplot(2,2,1), imshow(img);
+subplot(2,2,2), imshow(scaleImg1);
+subplot(2,2,3), imshow(scaleImg2);
+subplot(2,2,4), imshow(scaleImg3);
 %[bImg1,th1] = threshold(img,method="median");
 %[bImg2,th2] = threshold(img,method="mean");
 %[bImg3,th3] = threshold(img,method="otsu");
