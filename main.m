2ed8260b5e2f612c0f7b0fd91b599ac303abc2ac
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-09-18 17:41:21 +0800

    Implemented the bicubic interpolation for image resizing.

--- main.m
+++ main.m
@@ -4,16 +4,23 @@ addpath('./lib');
 # READING AND PROCESSING BITMAP IMAGE
 img = imread('charact2.bmp');
 img = img(:,:,1);
-imgStretch = stretchContrast(img);
-[imgEq,histEq] = histogramEqualize(img);
+%scaleImg1 = imageScale(img,targetRow=200,targetCol=400,method='nearestNeighbor');
+%scaleImg2 = imageScale(img,targetRow=200,targetCol=400,method='bilinear');
+scaleImg3 = imageScale(img,targetRow=200,targetCol=400,method='bicubic');
+scaleImg4 = imresize(img,[200,400],method='bicubic');
+%scaleImg = imageScale(rand(20,20),targetRow=50,targetCol=100,method='bicubic');
+
+%imgStretch = stretchContrast(img);
+%[imgEq,histEq] = histogramEqualize(img);
 %hist = histogram(img);
 %[bImg1,th1] = threshold(img,method="median");
 %[bImg2,th2] = threshold(img,method="mean");
 %[bImg3,th3] = threshold(img,method="otsu");
 %[bImg4,th4] = threshold(img,method="maxentropy");
-subplot(2,2,1), imshow(img);
-subplot(2,2,2), imshow(imgStretch);
-subplot(2,2,3), imshow(imgEq);
+%subplot(2,2,1), imshow(img);
+%subplot(2,2,2), imshow(scaleImg1);
+%subplot(2,2,3), imshow(scaleImg2);
+%subplot(2,2,4), imshow(scaleImg3);
 %imwrite(img)
 
 %count = histogram(img,normalize=0);
