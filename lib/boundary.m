24156ae83151c46a7719e5916760ededde68bde5
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-09-21 11:52:38 +0800

    Pad the input image for erosion and dilation.

--- lib/boundary.m
+++ lib/boundary.m
@@ -1,7 +1,9 @@
 function bImg = boundary(bImg)
     printf("Finding the boundary of binary image\n")
     [row,col] = size(img)
-    padImg = [img;img(row,:)];
-    padImg = [padImg padImg(:,col)];
+    padImg = [img(1,:);img];
+    padImg = [padImg(:,1) padImg];
+    padImg = [padImg;padImg(row+1,:)];
+    padImg = [padImg padImg(:,col+1)];
     
 endfunction
