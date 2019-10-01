4ac4319798dfb5edd4fdaf4184cd682ba3a5bdd1
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-10-01 14:21:19 +0800

    All individual parts tested. Rotation needs to be spaced nicely.

--- main.m
+++ main.m
@@ -1,4 +1,4 @@
-%clear;
+clear;
 addpath('./lib');
 pkg load image
 
@@ -7,17 +7,17 @@ img = imread('charact2.bmp');
 img = img(:,:,1);
 imgStretch = stretchContrast(img);
 [row,col] = size(img);
-imshow(img); input('Press enter to continue');
+%imshow(img); input('Press enter to continue');
 
 % 2. THRESHOLD THE IMAGE USING THE MEAN INTENSITY VALUE
 imgBlur_6 = imsmooth(img,'Gaussian',sigma=6);
 imgBlur_1 = imsmooth(img,'Gaussian',sigma=1);
-[bImg_6,th_6] = threshold(imgBlur_6,method='otsu'); imshow(bImg_6); input('Press enter to continue');
-[bImg_1_adaptive,th_1_adaptive] = thresholdAdaptive(imgBlur_1,method='mean',kernelSize=45); imshow(bImg_1_adaptive); input('Press enter to continue');
+[bImg_6,th_6] = threshold(imgBlur_6,method='otsu'); %imshow(bImg_6); input('Press enter to continue');
+[bImg_1_adaptive,th_1_adaptive] = thresholdAdaptive(imgBlur_1,method='mean',kernelSize=45); %imshow(bImg_1_adaptive); input('Press enter to continue');
 bImg = and(bImg_6,bImg_1_adaptive);
-bImg = binaryMorphology(bImg,method='open',kernelSize=5); imshow(bImg); input('Press enter to continue');
-bImg = binaryMorphology(bImg,method='close',kernelSize=3); imshow(bImg); input('Press enter to continue');
-bImg = particleAreaFilter(bImg,minArea=1000); imshow(bImg); input('Press enter to continue');
+bImg = binaryMorphology(bImg,method='open',kernelSize=5); %imshow(bImg); input('Press enter to continue');
+bImg = binaryMorphology(bImg,method='close',kernelSize=3); %imshow(bImg); input('Press enter to continue');
+bImg = particleAreaFilter(bImg,minArea=1000); %imshow(bImg); input('Press enter to continue');
 [labelImg,numLabel] = bwlabel(bImg,8);
 props = regionProps(labelImg);
 width = [];
@@ -31,15 +31,14 @@ for label = 1:numLabel
         bImg(props(label).BoundingBox(1):props(label).BoundingBox(2),props(label).Centroid(2)-1:props(label).Centroid(2)+1) = 0;
     endif
 endfor
-imshow(bImg); input('Press enter to continue');
+%imshow(bImg); input('Press enter to continue');
 
 % 3. SEGMENT ALL THE PARTICLES FORM ORIGINAL IMAGE
 [labelImg,numLabel] = bwlabel(bImg,8);
 props = regionProps(labelImg);
 for label = 1:numLabel
     cropImg = img(props(label).BoundingBox(1):props(label).BoundingBox(2),props(label).BoundingBox(3):props(label).BoundingBox(4));
-    imshow(cropImg)
-    input('Press enter to continue');
+    %imshow(cropImg); input('Press enter to continue');
 endfor
 
 % 4. ROTATE THE CHARACTERS ABOUT THEIR CENTER BY -90 DEGREES
@@ -53,7 +52,7 @@ for label = 1:numLabel
     startCol = int32(props(label).Centroid(2) - colRotCrop/2);
     newImg_rot_90(startRow:startRow+rowRotCrop-1,startCol:startCol+colRotCrop-1) = rotCropImg;
 endfor
-imshow(newImg_rot_90); input('Press enter to continue');
+%imshow(newImg_rot_90); input('Press enter to continue');
 %DISPLAY IMAGES HERE
 
 % 5. ROTATE THE CHARACTERS ABOUT THEIR CENTER BY 35 DEGREES
@@ -67,17 +66,17 @@ for label = 1:numLabel
     startCol = int32(props(label).Centroid(2) - colRotCrop/2);
     newImg_rot35(startRow:startRow+rowRotCrop-1,startCol:startCol+colRotCrop-1) = rotCropImg;
 endfor
-imshow(newImg_rot35); input('Press enter to continue');
+%imshow(newImg_rot35); input('Press enter to continue');
 %DISPLAY IMAGES HERE
 
 % 6. FIND THE OUTLINE OF SEGMENTED CHARACTERS
 bImgBdry = boundary(bImg);
-imshow(bImgBdry); input('Press enter to continue');
+%imshow(bImgBdry); input('Press enter to continue');
 %DISPLAY IMAGES HERE
 
 % 7. FIND THE SKELETON OF SEGMENTED CHARACTERS
 bImgSkeleton = skeleton(bImg);
-imshow(bImgSkeleton); input('Press enter to continue');
+%imshow(bImgSkeleton); input('Press enter to continue');
 % DISPLAY IMAGES HERE
 
 % 8. SCALE AND DISPLAY CHARACTERS 1A2B3C IN SEQUENCE
@@ -88,13 +87,13 @@ for label = 1:numLabel
     endif
 endfor
 finalImg = [];
-for label = {4,6,8,1,2,3,5,7,9,10,11,12,13}
+for label = {4,7,8,1,2,3,5,6,9,10,11,12,13}
     cropImg = imgStretch(props(label{1}).BoundingBox(1):props(label{1}).BoundingBox(2),props(label{1}).BoundingBox(3):props(label{1}).BoundingBox(4));
     targetCol = int32(props(label{1}).Width * targetRow / props(label{1}).Height);
     cropImgScale = imageScale(cropImg,targetRow=targetRow,targetCol=targetCol,method='bilinear');
     finalImg = [finalImg cropImgScale];
 endfor
-imshow(finalImg); input('Press enter to continue');
+%imshow(finalImg); input('Press enter to continue');
 %DISPLAY IMAGES HERE
 
 subplot(2,4,1), imshow(img)
