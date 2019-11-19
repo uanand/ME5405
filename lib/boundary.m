17a02228f3bc7d7d04d738fc9710a6f067dbf734
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-11-20 01:03:55 +0800

    Add help comments on how to use boundary.

--- lib/boundary.m
+++ lib/boundary.m
@@ -1,4 +1,17 @@
 function bImgBdry = boundary(bImg)
+% bImgBdry - Finds out the boundary of each object in a binary image.
+% 
+% Input parameters -
+%   bImg - binary image for which you need to find the boundary of the components
+% 
+% Usage
+% bImgBdry = boundary(bImg)
+%   To find the boundary, first bImg is eroded using a 3x3 kernel which looks like [[1,1,1],[1,1,1],[1,1,1]].
+%   Following this trhe eroded image is subtracted from the original binary image.
+%   All the pixels not are not a part of the boundary become 0 and the boundary pixels become 1
+%
+% Returns
+%   A new binary image with all the boundary pixels marked as 1
     [row,col] = size(bImg);
     bImgErode = zeros(row,col,'logical');
     bImgBdry = zeros(row,col,'logical');
@@ -7,14 +20,14 @@ function bImgBdry = boundary(bImg)
             product = bImg(r-1,c)*bImg(r,c-1)*bImg(r,c)*bImg(r,c+1)*bImg(r+1,c);
             if (product == 1)
                 bImgErode(r,c) = 1;
-            endif
-        endfor
-    endfor
+            end
+        end
+    end
     for r = 1:row
         for c = 1:col
             if (bImg(r,c)==1 && bImgErode(r,c)==0)
                 bImgBdry(r,c) = 1;
-            endif
-        endfor
-    endfor
-endfunction
+            end
+        end
+    end
+end
