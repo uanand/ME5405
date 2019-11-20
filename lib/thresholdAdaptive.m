6f60524defa548962299b33f25009bd390a46a6f
Utkarsh Anand <utkarsh.aanand@gmail.com>
2019-11-20 12:12:36 +0800

    Helper comments for usage of this function.

--- lib/thresholdAdaptive.m
+++ lib/thresholdAdaptive.m
@@ -1,6 +1,21 @@
 function [bImg,th] = thresholdAdaptive(img,method,kernelSize)
+% thresholdAdaptive -
+%   peform local thresholding for smaller square regions in the image. The size of the square region is defined by kernel size.
+% 
+% Input parameters -
+%   img - 8-bit grayscale image which has to be thresholded
+%   method - only mean threshold has been implemented now. We plan to implement other thresholding algorithms in the future. Select 'mean' now.
+%   kernelSize - should be an odd number. Size of the square structuring element that will be used for local thresholding.
+% 
+% Usage -
+% [bImg,th] = thresholdAdaptive(img,'mean',45);
+%   We select a ROI which is 45x45 pixels in size, and the average intensity valiu in ROI is calculated. If the intensity value of the center pixel of ROI is greater than the average it is marked as 1, and 0 otherwise.
+%
+% Returns -
+%   bImg. Binary image after performing local thresholding. It should be noted that the boundary region is always 0.
+%   th. The calculated threshold at every pixel based on ROI.
     [row,col] = size(img);
-    bImg = zeros(row,col,"logical");
+    bImg = zeros(row,col,'logical');
     th = zeros(row,col);
     
     if (mod(kernelSize,2))
